1936337cfb2bc0c28d44104852d6fbea442da974
Eric D. Triton <alexandersegerev@gmail.com>
2025-07-28 11:23:47 -0400

    Updated Navbar

--- capsi/src/components/Navbar.jsx
+++ capsi/src/components/Navbar.jsx
@@ -5,10 +5,11 @@ import './Navbar.css';
 
 function Navbar() {
   return (
+      
     <nav>
-        <a href="#" class="navbar-brand">
-    <img src={capsiLogo} alt="Your Logo" class="navbar-logo"/>
-  </a>
+      {/* The image is now a direct child of <nav> */}
+      <img src={capsiLogo} alt="CAPSI Logo" className="navbar-logo" />
+
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/industrial-action-plans">Industrial Action Plans</Link></li>
@@ -17,9 +18,13 @@ function Navbar() {
         <li><Link to="/otna-ethics">OTNA Ethics</Link></li>
         <li><Link to="/about">About OnTrackNorthAmerica</Link></li>
         <li><Link to="/braintrust">BrainTrust</Link></li>
+        
       </ul>
-    </nav>  
+
+    </nav>
+      
     );
+    
 }
 
 export default Navbar;
