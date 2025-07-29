c0872f76829cacfb0ae695edac91c680656ad26e
Eric D. Triton <alexandersegerev@gmail.com>
2025-07-29 15:58:29 -0400

    Added Regulations for OTNA

--- capsi/src/pages/OTNA-Ethics.jsx
+++ capsi/src/pages/OTNA-Ethics.jsx
@@ -3,13 +3,89 @@ import React from 'react'
 import '../App.css';
 
 
-function currVitae() {
+function otnaEthics() {
   return (
     <>
-    
+      <h1>The OnTrackNorthAmerica Promise, Ethics and Principles</h1>
+      <p>OnTrackNorthAmerica is a 501c3 non-profit that advises governments, businesses, and communities on sustainable industry, transportation, land use, and economic development.</p>
+
+      <h2>Promise Statement and Your Invitation</h2>
+      <p>OnTrackNorthAmerica promises to co-lead North America in redesigning its industrial systems to support sustainable life for humans and nature. You are invited to the team.</p>
+
+      <h2>Ethics</h2>
+      <h3>Objectivity</h3>
+      <p>OnTrackNorthAmerica does not accept financial sponsorships that require prioritization of individual business agendas over shared interests. This allows us to impartially evaluate the financial, environmental, and social viability of all projects and ideas.</p>
+
+      <h3>Inclusivity</h3>
+      <p>OnTrackNorthAmerica facilitates “level playing field” dialogues inviting representation and perspectives from all involved stakeholder groups. Inclusivity is fundamental to our planning approach, and we welcome all regions and projects of any size.</p>
+
+      <h3>Nonpartisanship</h3>
+      <p>OnTrackNorthAmerica maintains the autonomy of a politically non-partisan entity.</p>
+
+      <h3>Sustainability</h3>
+      <p>We believe return-on-investment analyses should account for all environmental and community impacts while acknowledging that some projects generate a degree of unavoidable negative impact. We support investments that advance the transition from the overuse of fossil fuels, petrochemicals, and water, while acknowledging the real-world challenges on the journey to a cleaner economy. We guide our clients and communities to align their planning and investment horizons with the sensible evolution of these markets.</p>
+
+    <p>There are four steps to evaluating every project for alignment with these values and goals:</p>
+    <h5>We catalog each project's commodities, activities, and impacts.</h5>
+    <h5>We conduct thorough research to gain an understanding of all environmental and community impacts.</h5>
+    <h5>We share our findings with the team and stakeholders to invite all perspectives.</h5>
+    <h5> Together, we decide how to influence the project's sustainability.</h5>
+    <p>We hold ourselves accountable to future generations by working only on projects that align with a sustainable environment and healthy communities.</p>
+
+    <h2>Principles for Collaboration</h2>
+    <h3>Collaboration and Coordination</h3>
+    <p>OTNA supports both thoughtful collaboration and coordination in balance with helpful competition. It is commonly believed that including many stakeholders hinders progress. In fact, incorporating all perspectives leads to smarter solutions, policies, and plans that are more effective, widely adopted, and implemented.</p>
+
+    <h3>Assume Responsibility</h3>
+    <p>We are responsible for the quality of our relationships. In each conversation, we relate in ways that advance mutual learning by recognizing the strengths and perspectives of others.</p>
+
+    <h3>Be Accountable</h3>
+    <p>Only make promises you intend to keep. Include a completion time. At first, we may face barriers to accomplishing new tasks. We only encounter challenges because of our commitment, not a lack of commitment. Stay in the game!</p>
+
+    <h3>Engage in Integrative Dialogue</h3>
+    <p>Integrative dialogue transcends conventional debate, where either/or and right/wrong interactions marginalize many valuable perspectives. We work together to synergize and foster new understandings, especially when seemingly opposing points of view are expressed.</p>
+
+    <h3>Ask Questions</h3>
+    <p>Asking questions when writing and speaking opens up new thinking and fosters new ideas. While statements tend to portray concerns as intractable problems, asking questions immediately orients us toward solutions.</p>
+
+    <h3>Thrive at the Edge of Learning</h3>
+    <p>What we are doing is unprecedented and requires us to continually learn and adapt. External change happens when we expand our receptiveness to new ideas. Bring it on!</p>
+
+    <h3>Embrace Knowledge Sharing</h3>
+    <p>Share rather than withhold information, ideas, and wisdom to create a world that works for everyone. Sharing accelerates productivity. It is time to scale up our collective intelligence.</p>
+
+    <h3>Celebrate and Energize</h3>
+    <p>Leaning into positivity is vitally important to any concerted effort. We value each other’s participation and communicate our appreciation.</p>
+
+    <h3>Lead from Community</h3>
+    <p>Initiatives succeed when strong leadership stewards the community’s best interests and stands for results.We advise project developers to interact proactively with community leaders as influential partners.</p>
+
+    <h3>Embrace Whole-System Thinking</h3>
+    <p>OnTrackNorthAmerica champions programs, policies, and planning that advance system-wide benefits and multimodal transportation.</p>
+
+    <h3>Step into IntelliSynthesis®</h3>
+    <p>IntelliSynthesis® is the logic and intelligence all around us, but so often hidden from view by posturing, positioning, and politicizing. When we engage with others in the community’s best interest, collective intelligence naturally moves us toward solutions that work for everyone.</p>
+
+  <h2>Your Opportunity to Promise</h2>
+
+  <p>Indicate each promise that fits for you: <br/>
+☐ I promise to uphold OnTrackNorthAmerica’s principles and ethics and contribute to its goals while on the OTNA Team. <br/>
+
+☐ I promise to align my economic activities to align with OTNA’s principles and ethics, supporting the redesign of industrial systems to ensure sustainable life on Earth.<br/>
+
+☐ I promise to support society in redesigning its industrial systems to ensure sustainable life on Earth.<br/>
+
+☐ I promise to co-lead society in redesigning its industrial systems to ensure sustainable life on Earth.<br/>
+
+Signature _______________________________________ Date________________________ <br/>
+
+Name _______________________________________
+
+</p>
+
     </>
     
   );
 }
 
-export default currVitae;
+export default otnaEthics;
